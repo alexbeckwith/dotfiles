924311117077ef3c3b8ab0b04a25cbff06d717bd
Alex Beckwith <abeckwith@churchcommunitybuilder.com>
2017-01-18 14:27:11 -0700

    Add vim config

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ killall Finder
 
 dir=~/dotfiles
 zshconf=~/.zshrc
+vimconf=~/vim
 sublconf=User
 
 echo "Moving zsh settings"
@@ -23,6 +24,9 @@ else
 fi
 echo "Creating symlink for zsh settings"
 ln -sf $dir/zsh/zshrc $zshconf
+
+echo "Creating symlink for vim settings"
+ln -sf $dir/vim/.vimrc $vimconf
 
 echo "Moving Sublime user settings"
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
@@ -38,5 +42,4 @@ mkdir  $dir/zsh/oh-my-zsh/custom/themes
 ln -sf $dir/zsh/bullet-train-oh-my-zsh-theme/bullet-train.zsh-theme $dir/zsh/oh-my-zsh/custom/themes/bullet-train.zsh-theme
 ln -sf $dir/zsh/zsh-syntax-highlighting $dir/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ln -sf $dir/zsh/zsh-autosuggestions $dir/zsh/oh-my-zsh/custom/plugins/zsh-autosuggestions
-
 
